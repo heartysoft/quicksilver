7cad0775a82ac17b42fa6e0a6e0af8d8a65feb7f
Ashic Mahtab (MC) <ashic@live.com>
2015-02-19 14:57:58 +0000

    initial drop

---
+++ MyWebAppForDeployment/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace MyWebAppForDeployment.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ MyWebAppForDeployment/Areas/HelpPage/ModelDescriptions/EnumValueDescription.cs
@@ -0,0 +1,11 @@
+namespace MyWebAppForDeployment.Areas.HelpPage.ModelDescriptions
+{
+    public class EnumValueDescription
+    {
+        public string Documentation { get; set; }
+
+        public string Name { get; set; }
+
+        public string Value { get; set; }
+    }
+}
