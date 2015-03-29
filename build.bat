9428bd1f0c1b2820c4e589c8894377f0be148942
ashic <ashic@live.com>
2015-03-29 22:15:26 +0100

    slightly better qs.bat example

--- build.bat
+++ build.bat
@@ -2,15 +2,16 @@
 SETLOCAL
 ::set to packages dir. Must NOT contain trailing \
 SET _packages=".\packages"
+SET _nuget=".nuget\NuGet.exe"
 
 cls
 
 if not exist "%_packages%\fake" (
-".nuget\NuGet.exe" "Install" "FAKE" "-OutputDirectory" "%_packages%" "-ExcludeVersion"
+%_nuget% "Install" "FAKE" "-OutputDirectory" "%_packages%" "-ExcludeVersion"
 )
 
 if not exist "%_packages%\config-transform" (
-".nuget\NuGet.exe" "Install" "config-transform" -pre "-OutputDirectory" "%_packages%" "-ExcludeVersion"
+%_nuget% "Install" "config-transform" -pre "-OutputDirectory" "%_packages%" "-ExcludeVersion"
 )
 
 "%_packages%\FAKE\tools\Fake.exe" build.fsx %*
--- qs.bat
+++ qs.bat
@@ -2,19 +2,20 @@
 SETLOCAL
 ::set to packages dir. Must NOT contain trailing \
 SET _packages=".\packages"
+SET _nuget=".nuget\NuGet.exe"
 
 cls
 
 if not exist "%_packages%\fake" (
-".nuget\NuGet.exe" "Install" "FAKE" "-OutputDirectory" "%_packages%" "-ExcludeVersion"
+%_nuget% "Install" "FAKE" "-OutputDirectory" "%_packages%" "-ExcludeVersion"
 )
 
 if not exist "%_packages%\config-transform" (
-".nuget\NuGet.exe" "Install" "config-transform" -pre "-OutputDirectory" "%_packages%" "-ExcludeVersion"
+%_nuget% "Install" "config-transform" -pre "-OutputDirectory" "%_packages%" "-ExcludeVersion"
 )
 
 if not exist "%_packages%\quicksilver" (
-".nuget\NuGet.exe" "Install" "quicksilver" -pre "-OutputDirectory" "%_packages%" "-ExcludeVersion"
+%_nuget% "Install" "quicksilver" -pre "-OutputDirectory" "%_packages%" "-ExcludeVersion"
 )
 
 "%_packages%\FAKE\tools\Fake.exe" build.fsx %*
