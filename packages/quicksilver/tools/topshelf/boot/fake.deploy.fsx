2e2e030f143e6b45d87c3b54214332485f36f996
Ashic Mahtab (MC) <ashic@live.com>
2015-04-17 11:32:51 +0100

    better output during servive install

--- packages/quicksilver/tools/topshelf/boot/fake.deploy.fsx
+++ packages/quicksilver/tools/topshelf/boot/fake.deploy.fsx
@@ -8,19 +8,26 @@ let envOpt =
 
 printfn "starting installer via install.bat."
 
-let p = new ProcessStartInfo("install.bat")
-p.WorkingDirectory <- __SOURCE_DIRECTORY__
+let p = new ProcessStartInfo("cmd.exe")
 p.UseShellExecute <- false
+p.RedirectStandardOutput <- true
+p.RedirectStandardError <- true
+p.WorkingDirectory <- __SOURCE_DIRECTORY__
 
 match envOpt with
 | Some(e) -> 
     let env = e.Substring(e.IndexOf("=") + 1)
-    p.Arguments <- env
+    p.Arguments <- sprintf "/c install.bat %s" env
     printfn "environment: %s." env
 | None -> 
+    p.Arguments <- "/c install.bat"
     printfn "environment not specified."
 
 let pr = Process.Start(p)
+pr.OutputDataReceived.Add(fun args -> printfn "Info: %A" args.Data)
+pr.ErrorDataReceived.Add(fun args -> System.Console.Error.WriteLine("Error: {0}", args.Data))
+pr.BeginOutputReadLine();
+pr.BeginErrorReadLine();
 
 pr.WaitForExit()
 
