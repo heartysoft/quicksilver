89f0623dd41195bf0c7ef482d6ac9692d6ee69c6
Ashic Mahtab (MC) <ashic@live.com>
2015-04-21 11:19:44 +0100

    updated website deploy.fsx

--- packages/quicksilver/tools/website/boot/fake.deploy.fsx
+++ packages/quicksilver/tools/website/boot/fake.deploy.fsx
@@ -1,10 +1,55 @@
 open System
 open System.Diagnostics
+open System.IO
 
 let envOpt = 
     fsi.CommandLineArgs 
     |> Seq.tryFind (fun arg -> arg.StartsWith("env="))
 
+let env = 
+    Option.map (fun (e:string) -> e.Substring(e.IndexOf("=") + 1)) envOpt
+
+printfn "Step: iis initialisation"
+printfn "########################################################"
+
+let iisFile = 
+    match env with
+    | Some(e) -> sprintf "iis.%s.pson" e
+    | None -> "iis.pson"
+
+if File.Exists iisFile then
+    printfn "IIS file %s found. Running..." iisFile
+    let p = new ProcessStartInfo("powershell.exe")
+    p.UseShellExecute <- false
+    p.RedirectStandardOutput <- true
+    p.RedirectStandardError <- true
+    p.WorkingDirectory <- __SOURCE_DIRECTORY__
+    p.Arguments <- sprintf "-ExecutionPolicy RemoteSigned website_setup.ps1 %s" iisFile
+
+    printfn "ensuring iis setup matches %s" iisFile
+
+    let pr = Process.Start(p)
+    pr.OutputDataReceived.Add(fun args -> printfn "%A" args.Data)
+    pr.ErrorDataReceived.Add(fun args -> System.Console.Error.WriteLine(args.Data))
+    pr.BeginOutputReadLine();
+    pr.BeginErrorReadLine();
+
+    pr.WaitForExit()
+
+    let exitCode = pr.ExitCode
+
+    printfn "iis initialisation script finished. Exit code: %A" exitCode
+
+    if exitCode <> 0 then
+        failwith "iis initialisation failed"
+    else
+        printfn "iis initialisation complete"
+else
+    printfn "IIS file server(.%s).pson not found. Skipping iis initialisation." iisFile
+
+printfn "########################################################"
+printfn "step: website installation"
+printfn "########################################################"
 
 printfn "starting installer via install.bat."
 
@@ -16,9 +61,8 @@ p.WorkingDirectory <- __SOURCE_DIRECTORY__
 
 match envOpt with
 | Some(e) -> 
-    let env = e.Substring(e.IndexOf("=") + 1)
-    p.Arguments <- sprintf "/c install.bat %s /Y" env
-    printfn "environment: %s." env
+    p.Arguments <- sprintf "/c install.bat %s /Y" env.Value
+    printfn "environment: %s." env.Value
 | None -> 
     p.Arguments <- "/c install.bat /Y"
     printfn "environment not specified."
